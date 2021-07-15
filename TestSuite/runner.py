3b3bb2b97300e0e89057e94b3e42d334df913294
maniuniuer <1837463487@qq.com>
2021-07-15 14:37:31 +0800

    newfile

---
+++ TestSuite/runner.py
@@ -0,0 +1,14 @@
+from tt.实例练习.TestSuite.Page.Base import base
+from tt.实例练习.TestSuite.pageobject.SearcH import Searchpage
+from selenium.webdriver.common.by import By
+#from TestSuite.Variablelayer.Variable import *
+import time
+import unittest
+leave='成都'
+leave_data="2021-07-20"
+arrive='北京'
+arrive_data='2021-07-30'
+aa=Searchpage()
+aa.search7(leave='成都',leave_data="2021-07-20",arrive='北京',arrive_data='2021-07-30')
+
+
